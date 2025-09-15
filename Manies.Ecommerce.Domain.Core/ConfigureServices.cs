d5e7743fe50c8adf90a27ffcac640d60e6accd58
Nicol <nicolas.mannarino@hotmail.com>
2025-09-15 16:03:36 -0300

    Agrego clase ConfigureServices con inyección de dependencias

---
+++ Manies.Ecommerce.Domain.Core/ConfigureServices.cs
@@ -0,0 +1,14 @@
+﻿using Manies.Ecommerce.Domain.Interface;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Manies.Ecommerce.Domain.Core
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddDomainServices(this IServiceCollection services)
+        {
+            services.AddScoped<ICustomerDomain, CustomerDomain>();
+            return services;
+        }
+    }
+}
