07e1f51427ccfe9ae060c7b478db65c7d6a8019b
Nicol <nicolas.mannarino@hotmail.com>
2025-09-18 00:44:29 -0300

    Extension e Inyeccion de dependencias en la capa de aplicacion

---
+++ Manies.Ecommerce.Application.Main/ConfigureServices.cs
@@ -0,0 +1,17 @@
+﻿using Manies.Ecommerce.Application.Interface;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Manies.Ecommerce.Application.Main
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<ICustomersApplication, CustomersApplication>();
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
