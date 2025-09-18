44e85dfd154c1fb965901496af1bf65d2c3d40d8
Nicol <nicolas.mannarino@hotmail.com>
2025-09-17 22:21:29 -0300

    Metodos de Extension e Inyeccion de Dependencias en la Capa de Infraestructura

---
+++ Manies.Ecommerce.Infrastructure.Repository/ConfigureServices.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Manies.Ecommerce.Infrastructure.Data;
+using Manies.Ecommerce.Infrastructure.Interface;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Manies.Ecommerce.Infrastructure.Repository
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddSingleton<DapperContext>();
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+    }
+}
